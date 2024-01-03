7767f27646ab0c1d552ad82f853f3cbf5cedef1e
hackolite <laureote-loic@hotmail.fr>
2024-01-03 21:52:21 +0100

    clean craft

--- craft/detect.py
+++ craft/detect.py
@@ -5,25 +5,15 @@ import PIL
 from PIL import Image
 import os
 import time
-detector = keras_ocr.detection.Detector()
-detector.model.load_weights("ssd_2.h5")
-
-PATHTEST = "./TEST"
-RESULT  = "./RES_2"
 
 
-for i in os.listdir(PATHTEST):
-    if "jpeg" or "jpg" in i:
-        start = time.time()
-        image = keras_ocr.tools.read(PATHTEST+"/"+i)
-        h, w, _ = image.shape
-        w = w * 2
-        if h > 640:
-            h=640
-        if w > 640:
-            w=320 
-        image = cv2.resize(image, dsize=(h, w), interpolation=cv2.INTER_CUBIC)
-        boxes = detector.detect(images=[image])[0]
-        drawn = keras_ocr.tools.drawBoxes(image=image, boxes=boxes)
-        cv2.imwrite(RESULT+"/"+i, drawn)
-        print(time.time() - start) 
+
+def model_constructor():
+    pass
+
+
+def execute(model_path=None, image=None, config=None):
+    detector = keras_ocr.detection.Detector()
+    detector.model.load_weights(model_path)
+    boxes = detector.detect(images=[image])[0]
+    return {"image":image, "boxes":boxes}
