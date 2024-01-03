72d1477c431daa2e7697f8e4d1f1d46df1518fad
hackolite <laureote-loic@hotmail.fr>
2024-01-03 19:32:08 +0100

    clean faster_rcnn

--- faster_rcnn/detection.py
+++ faster_rcnn/detection.py
@@ -15,17 +15,18 @@ import cv2
 from PIL import Image
 
 #https://pseudo-lab.github.io/Tutorial-Book-en/chapters/en/object-detection/Ch4-RetinaNet.html
-anchor_generator = AnchorGenerator((16, 32, 64, 128, 256, 512), (0.1, 0.2, 0.4, 0.7, 1))
-retina = torchvision.models.detection.retinanet_resnet50_fpn(num_classes = 2, pretrained=False, pretrained_backbone = True, rpn_anchor_generator=anchor_generator)
+anchor_generator = AnchorGenerator(sizes=(32, 64, 128, 256, 512),aspect_ratios=(0.025, 0.05, 1.0)) 
+retina = torchvision.models.detection.fasterrcnn_resnet50_fpn(num_classes = 2, pretrained=False, pretrained_backbone = True, rpn_anchor_generator=anchor_generator)
 device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 retina.to(device)
-retina.load_state_dict(torch.load("./retina_price_4.pt"))
+retina.load_state_dict(torch.load("./faster_rcnn_model.pth"))
 
 
 
 def make_prediction(model, img, threshold):
     model.eval()
     preds = model(img)
+    print(preds)
     for id in range(len(preds)) :
         idx_list = []
 
@@ -37,10 +38,11 @@ def make_prediction(model, img, threshold):
         preds[id]['labels'] = preds[id]['labels'][idx_list]
         preds[id]['scores'] = preds[id]['scores'][idx_list]
 
+    
     return preds
 
 with torch.no_grad():
-        im = cv2.imread("zeycpsweoi.jpg")
+        im = cv2.imread("im.jpg")
         # You may need to convert the color.
         img_data = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
         im_pil = Image.fromarray(img_data)
@@ -51,6 +53,5 @@ with torch.no_grad():
         preds = make_prediction(retina, im, 0.5)
         list_of_boxes = preds[0]["boxes"].tolist()
         for box in list_of_boxes:
-            #print((box[3]-box[1])/(box[2]-box[0]))
             cv2.rectangle(img_data,(int(box[0]),int(box[1])),(int(box[2]),int(box[3])),(0,255,0),3)
         cv2.imwrite("test.jpg", img_data)
--- retinanet/train.py
+++ retinanet/train.py
@@ -1,5 +1,4 @@
 import torch
-    
 from torchvision.models.detection.retinanet import RetinaNetHead
 from torchvision.models.detection.anchor_utils import AnchorGenerator
 from torchvision.models.detection import _utils as det_utils
@@ -11,6 +10,10 @@ from torch.optim import SGD
 import torchvision
 from dataset import TextDataset
 import time 
+from functools import partial
+import torchvision.transforms as transforms
+from tqdm import tqdm 
+
 
 #https://pseudo-lab.github.io/Tutorial-Book-en/chapters/en/object-detection/Ch4-RetinaNet.html
 
@@ -19,43 +22,58 @@ def collate_fn(batch):
     return tuple(zip(*batch))
 
 
-torch.cuda.empty_cache()
+# Définissez la transformation pour le dataset
+#transform = transforms.Compose([
+#    transforms.GaussianBlur(kernel_size=501),
+#    transforms.RandomRotation(degrees=180),
+#    transforms.RandomHorizontalFlip(p=0.5),
+#    transforms.Resize((300,300)),
+#    transforms.ToTensor(),  # Convertir l'image en un tensor
+#    # Ajoutez d'autres transformations si nécessaire
+#])
 
-dataset = TextDataset("../PAD/trainval.txt", image_dir="../PAD")
+# Définissez la transformation pour le dataset
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Resize((300,300))  # Convertir l'image en un tensor
+])
+
+
+dataset = TextDataset("../PAD/trainval.txt", image_dir="../PAD",transform=transform)
 dataset.filter()
-data_loader = torch.utils.data.DataLoader(dataset, batch_size=1, collate_fn=collate_fn)
+data_loader = torch.utils.data.DataLoader(dataset, batch_size=1, collate_fn=collate_fn, shuffle=True)
 
+anchor_generator = AnchorGenerator((16, 32, 64, 128, 256, 512), (0.1, 0.2, 0.4, 0.7, 1))
 
-retina = torchvision.models.detection.retinanet_resnet50_fpn(num_classes = 1, pretrained=False, pretrained_backbone = True)
+retina = torchvision.models.detection.retinanet_resnet50_fpn(num_classes=2, pretrained=False, pretrained_backbone = True, rpn_anchor_generator=anchor_generator)
+
+retina.load_state_dict(torch.load("./retina_price_4.pt"))
 device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-
-num_epochs = 5
+num_epochs = 10
 retina.to(device)
     
 # parameters
 params = [p for p in retina.parameters() if p.requires_grad] # select parameters that require gradient calculation
 optimizer = torch.optim.SGD(params, lr=0.005, momentum=0.9, weight_decay=0.0005)
-
+#optimizer = torch.optim.Adam(retina.parameters(), lr=0.01)
 
 
 # about 4 min per epoch on Colab GPU
 for epoch in range(num_epochs):
-    start = time.time()
     retina.train()
-
-    i = 0    
     epoch_loss = 0
-    for images, targets in data_loader:
+    for images, targets in tqdm(data_loader):
         images = list(image.to(device) for image in images)
         targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
         loss_dict = retina(images, targets) 
         losses = sum(loss for loss in loss_dict.values()) 
-        i += 1
         optimizer.zero_grad()
         losses.backward()
+        torch.nn.utils.clip_grad_norm_(retina.parameters(), 0.1)
         optimizer.step()
-        print(torch.cuda.memory_allocated())
         epoch_loss += losses
-    print(epoch_loss, f'time: {time.time() - start}')
+        loss_metric = losses.tolist()
+        print(loss_metric)
+    print(f'Epoch {epoch}/{num_epochs}, Loss: {loss_metric}')
 
 torch.save(retina.state_dict(),f'retina_{num_epochs}.pt')
